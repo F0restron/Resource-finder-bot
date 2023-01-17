ff3ba25ea4fd9476586ea12f53e9b27f3fbaf5e6
F0restron <122814005+F0restron@users.noreply.github.com>
2023-01-17 09:55:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+token = '5480035432:AAFYh9GMtoNcB5o5koJNYM56_8bFX0BAZ7A'
+
+welcome_msg = """Thanks for using me 😊
+
+I'll provide you with free courses on various topics for free 😁. You can request your favorite course & I am updated every week with new courses by the admin.
+
+Enjoy 🎉🔥
+"""
+
+sem4_DBMS='https://github.com/flick-23/SEM-4/tree/main/DBMS'
+sem4_PYTHON='https://github.com/flick-23/SEM-4/tree/main/PYTHON'
+sem4_DAA='https://github.com/flick-23/SEM-4/tree/main/DAA' 
+sem4_OS='https://github.com/flick-23/SEM-4/tree/main/OS'
+sem4_MATHS='https://github.com/flick-23/SEM-4/tree/main/MATHS' 
+sem4_SE='https://github.com/flick-23/SEM-4/tree/main/SE'
+sem4_EXAMS='https://github.com/flick-23/SEM-4/tree/main/Exams' 
+sem4_JOURNALS='https://github.com/flick-23/SEM-4/tree/main/Journals'
+    
+sem3_CO='https://github.com/flick-23/SEM-3/tree/main/Computer%20Organization' 
+sem3_DE='https://github.com/flick-23/SEM-3/tree/main/Digital%20Electronics'
+sem3_DS='https://github.com/flick-23/SEM-3/tree/main/Data%20Structures' 
+sem3_MATHS='https://github.com/flick-23/SEM-3/tree/main/Mathematics'
+sem3_JAVA='https://github.com/flick-23/SEM-3/tree/main/OOPS%20with%20Java' 
+sem3_EXAMS='https://github.com/flick-23/SEM-3/tree/main/Exams'
+sem3_WP='https://github.com/flick-23/SEM-3/tree/main/Web%20Programming'
+
+url_INTERNSHIP = 'https://docs.google.com/spreadsheets/d/1gtMreNBHoXFAEh7BdhMByAUbR7ORPvXbGw_sN68YRFA/edit'
+
+c_WEB_DEVELOPMENT = 'https://mega.nz/folder/Hs8FTYjA#95FWIREJGWVz8-uaUlhFgw'
+c_JAVASCRIPT = 'https://mega.nz/folder/t45ngQJJ#OYq7XPvwf6mD590NgS_l1w'
+c_STOCKMARKET = 'https://youtu.be/VuJURlJ-S24'
+c_CRYPTO = 'https://mega.nz/folder/6GwzWDyI#S5mJK_DyXcUZrXrShqzqdA'
+c_C_plus = 'https://drive.google.com/drive/folders/1q2K4J7zKoWm94mbJj4WYOQT3uR6XSa4_'
+c_JAVA = 'https://mega.nz/folder/ms5WHQKZ#pa0E70PgHzBTUK-MN23tZw'
+
+cocuricular='nfdjnfjdn'
+cocuricular_valorant='djfnwjefnwj'
+cocuricular_csgo='djfnwwj'
+n_nptel='https://nptel.ac.in/'
